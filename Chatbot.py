7d0720de8497c79573a5fdf9b2f9f609aab8a84e
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-26 21:28:35 +0300

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,8 +1,8 @@
 import openai
 import streamlit as st
-
+API_KEY=st.secrets["openAI_key"]
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
@@ -16,11 +16,8 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
 
-    openai.api_key = openai_api_key
+    openai.api_key = API_KEY
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
