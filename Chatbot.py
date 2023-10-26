cd50559c6f9299a1748a350aede19576fc877c96
CakeLiar <79081062+CakeLiar@users.noreply.github.com>
2023-10-26 22:43:55 +0300

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -71,9 +71,8 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    print(prompt)
-    st.write(str(semantic_search('hello world')))
     openai.api_key = API_KEY
+    st.session_state.messages.append({"role": 'system', 'content': 'the following is your memory: '+str(semantic_search(prompt))})
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
