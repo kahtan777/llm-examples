7a00e1886c1fc5c3b9f4597861ed3818724a30ea
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-26 22:50:05 +0300

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -16,34 +16,33 @@ P_API_KEY =st.secrets["pincone_key"]
 API_KEY=st.secrets["openAI_key"]
 st.set_page_config(layout="centered")
 
-with st.container():
-    video_html = """
-    <style>
-    .video-container {
-    width: 100%; 
-    height: auto;
-    }
+video_html = """
+<style>
+.video-container {
+width: 100%; 
+height: auto;
+}
 
-    video {
-    width: 100%; 
-    height: auto;
-    }
+video {
+width: 100%; 
+height: auto;
+}
 
-    .content {
-    background: rgba(0, 0, 0, 0.5);
-    color: #f1f1f1;
-    width: 100%;
-    padding: 20px;
-    }
-    </style>    
-    <div class="video-container">
-    <video autoplay muted loop id="myVideo">
-        <source src="https://static.streamlit.io/examples/star.mp4">
-        Your browser does not support HTML5 video.
-    </video>
-    </div>
-    """  
-    st.markdown(video_html, unsafe_allow_html=True) 
+.content {
+background: rgba(0, 0, 0, 0.5);
+color: #f1f1f1;
+width: 100%;
+padding: 20px;
+}
+</style>    
+<div class="video-container">
+<video autoplay muted loop id="myVideo">
+    <source src="https://static.streamlit.io/examples/star.mp4">
+    Your browser does not support HTML5 video.
+</video>
+</div>
+"""  
+st.markdown(video_html, unsafe_allow_html=True) 
 
 loader = WebBaseLoader("https://medium.com/swlh/an-ultimate-guide-to-creating-a-startup-3b310f41d7e7")
 data = loader.load()
