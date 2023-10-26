710c865802bce25e82af7fc67abd73dd65db44a4
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-26 22:52:56 +0300

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -99,7 +99,7 @@ for msg in st.session_state.messages:
 if prompt := st.chat_input():
     openai.api_key = API_KEY
     st.session_state.messages.append({"role": 'system', 'content': 'the following is your memory: '+str(semantic_search(prompt))})
-    
+    st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     msg = response.choices[0].message
